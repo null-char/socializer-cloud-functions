b01c3f1fcfbc3a79a51e52107d8274920b34c12a
SKP <skp.frl@gmail.com>
2020-01-25 08:19:45 +0530

    move: Move firebase config to a separate file

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,23 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express';
 import * as firebase from 'firebase';
+import { config } from './utils/config';
 import { getPosts, addPost } from './handlers/posts';
 import { signUp, signIn } from './handlers/user';
 import { tokenAuth } from './utils/tokenAuth';
 
 const app = express();
-const config = {
-  apiKey: 'AIzaSyCN6y89ruVX7xESbDjp6UXwBJ7f5Z7sEi8',
-  authDomain: 'socializer-e77ce.firebaseapp.com',
-  databaseURL: 'https://socializer-e77ce.firebaseio.com',
-  projectId: 'socializer-e77ce',
-  storageBucket: 'socializer-e77ce.appspot.com',
-  messagingSenderId: '155031946649',
-  appId: '1:155031946649:web:8c9494f2a17cc80bdfff08',
-  measurementId: 'G-QL72HQ5GHC'
-};
 firebase.initializeApp(config);
 
 app.get('/posts', getPosts);
-
 app.use('/posts', tokenAuth);
-
 app.post('/posts', addPost);
 
 // SIGN UP
 // We'll do stuff like checking if email is not blank or if password is a certain length in the client side code
 
 app.post('/signup', signUp);
-
 app.post('/signin', signIn);
-
 // temporary. should be done on the client
 app.post('/signout', async (req, res) => {
   try {
---
+++ src/utils/config.ts
@@ -0,0 +1,10 @@
+export const config = {
+  apiKey: 'AIzaSyCN6y89ruVX7xESbDjp6UXwBJ7f5Z7sEi8',
+  authDomain: 'socializer-e77ce.firebaseapp.com',
+  databaseURL: 'https://socializer-e77ce.firebaseio.com',
+  projectId: 'socializer-e77ce',
+  storageBucket: 'socializer-e77ce.appspot.com',
+  messagingSenderId: '155031946649',
+  appId: '1:155031946649:web:8c9494f2a17cc80bdfff08',
+  measurementId: 'G-QL72HQ5GHC'
+};
