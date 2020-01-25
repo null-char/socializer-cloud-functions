1c00e746fb486f9338c8fe2ac912736e57b110d7
SKP <skp.frl@gmail.com>
2020-01-25 23:40:25 +0530

    add: Add likePost handler and addComment handler

--- src/handlers/posts.ts
+++ src/handlers/posts.ts
@@ -27,13 +27,49 @@ export const addPost: RequestHandler = async (req, res) => {
   };
 
   try {
-    const docRef = await db.collection('posts').add(newPost);
+    await db
+      .collection('posts')
+      .doc(newPost.createdAt)
+      .set(newPost);
     return res.json({
-      message: `Document added successfully with id ${docRef.id}`
+      message: `Document added successfully with id ${newPost.createdAt}`
     });
   } catch (err) {
     return res
       .status(500)
       .json({ error: 'Something went wrong with the server' });
+  }
+};
+
+export const likePost: RequestHandler = async (req, res) => {
+  try {
+    await db
+      .doc(`posts/${req.body.postId}`)
+      .update({ likes: { userHandle: req.body.userHandle } });
+    res
+      .status(200)
+      .json({
+        message: `Like added successfully to post with post id ${req.body.postId}`
+      });
+  } catch (err) {
+    res.status(500).json({ error: 'Server error' });
+  }
+};
+
+export const addComment: RequestHandler = async (req, res) => {
+  try {
+    await db.doc(`posts/${req.body.postId}`).update({
+      comments: {
+        userHandle: req.body.userHandle,
+        body: req.body.commentBody
+      }
+    });
+    res
+      .status(200)
+      .json({
+        message: `Comment added successfully to post with post id ${req.body.postId}`
+      });
+  } catch (err) {
+    res.status(500).json({ error: 'Server error' });
   }
 };
