513b30eeb52558fdb766bde05bf25d6e67cc9a75
SKP <skp.frl@gmail.com>
2020-01-25 13:26:00 +0530

    add: Provide credentials to config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,9 +1,14 @@
+import * as admin from 'firebase-admin';
+
 export const config = {
   apiKey: 'AIzaSyCN6y89ruVX7xESbDjp6UXwBJ7f5Z7sEi8',
   authDomain: 'socializer-e77ce.firebaseapp.com',
   databaseURL: 'https://socializer-e77ce.firebaseio.com',
   projectId: 'socializer-e77ce',
   storageBucket: 'socializer-e77ce.appspot.com',
+  credential: admin.credential.cert(
+    'C:/Users/S-KIRAN/Desktop/socializer_credentials.json'
+  ),
   messagingSenderId: '155031946649',
   appId: '1:155031946649:web:8c9494f2a17cc80bdfff08',
   measurementId: 'G-QL72HQ5GHC'
