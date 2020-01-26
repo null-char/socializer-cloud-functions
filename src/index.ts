f8962851aaa3201bd92f290d36da26519cc04e3a
SKP <skp.frl@gmail.com>
2020-01-26 22:44:02 +0530

    feature: Add follow user, like post, set user data endpoints

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,38 @@ import * as functions from 'firebase-functions';
 import * as express from 'express';
 import * as firebase from 'firebase';
 import { config } from './utils/config';
-import { getPosts, addPost } from './handlers/posts';
-import { signUp, signIn, uploadUserImage } from './handlers/user';
+import {
+  signUp,
+  signIn,
+  uploadUserImage,
+  setUserData,
+  followUser,
+  unfollowUser,
+  getAllPosts,
+  getPost,
+  addPost,
+  removePost,
+  likePost,
+  unlikePost,
+  addComment,
+  removeComment
+} from './handlers/index';
 import { tokenAuth } from './utils/tokenAuth';
 
 const app = express();
 firebase.initializeApp(config);
 
-app.get('/posts', getPosts);
-app.use('/posts', tokenAuth);
-app.post('/posts', addPost);
+app.get('/posts', getAllPosts);
+app.get('/posts/:postId', getPost);
+
+app.post('/posts', tokenAuth, addPost);
+app.delete('/posts/:postId', tokenAuth, removePost);
+
+app.post('/posts/likePost', tokenAuth, likePost);
+app.post('/posts/unlikePost', tokenAuth, unlikePost);
+
+app.post('/posts/addComment', tokenAuth, addComment);
+app.delete('/posts/removeComment', tokenAuth, removeComment);
 
 // SIGN UP
 // We'll do stuff like checking if email is not blank or if password is a certain length in the client side code
@@ -29,5 +51,8 @@ app.post('/signout', async (req, res) => {
 });
 
 app.post('/user/image', tokenAuth, uploadUserImage);
+app.post('/user', tokenAuth, setUserData);
+app.post('/user/follow', tokenAuth, followUser);
+app.delete('/user/unfollow/:userHandle', tokenAuth, unfollowUser);
 
 export const api = functions.https.onRequest(app);
