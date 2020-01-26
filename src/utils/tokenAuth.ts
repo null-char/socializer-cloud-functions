7f4c60f3fc6b01b28dc0ab7bf7212334018e9e85
SKP <skp.frl@gmail.com>
2020-01-26 13:20:08 +0530

    refactor: Refactor authentication of token

--- src/utils/tokenAuth.ts
+++ src/utils/tokenAuth.ts
@@ -3,32 +3,29 @@ import { db } from './admin';
 import { RequestHandler } from 'express';
 
 export const tokenAuth: RequestHandler = async (req, res, next) => {
-  // only execute this middleware function if the request method is POST
-  if (req.method === 'POST') {
-    if (
-      req.headers.authorization &&
-      req.headers.authorization.startsWith('Bearer ')
-    ) {
-      const token = req.headers.authorization.split('Bearer ')[1];
-      // Verify token
-      try {
-        const decodedToken = await admin.auth().verifyIdToken(token);
-        const userDoc = await db
-          .collection('users')
-          .where('userId', '==', decodedToken.uid)
-          .limit(1)
-          .get();
-        req.body.userHandle = userDoc.docs[0].data().userHandle;
-        return next();
-      } catch (err) {
-        res.status(403).json(err);
-      }
-    } else {
-      res
-        .status(403)
-        .json({ error: 'Invalid token or no authorization header provided' });
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer ')
+  ) {
+    // token is after the "Bearer ". split the string to get the token
+    const token = req.headers.authorization.split('Bearer ')[1];
+    // Verify token
+    try {
+      const decodedToken = await admin.auth().verifyIdToken(token);
+      const userDoc = await db
+        .collection('users')
+        .where('userId', '==', decodedToken.uid)
+        .limit(1)
+        .get();
+      req.body.user = userDoc.docs[0].data();
+      return next();
+    } catch (err) {
+      // not authorized
+      res.status(403).json(err);
     }
   } else {
-    return next();
+    res
+      .status(403)
+      .json({ error: 'Invalid token or no authorization header provided' });
   }
 };
